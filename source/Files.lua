e57a9761e2dc687be18b8090379a2a4864a400f6
softbf395 <130502864+softbf395@users.noreply.github.com>
2024-10-10 15:05:03 -0500

    Create Files.lua

---
+++ source/Files.lua
@@ -0,0 +1,33 @@
+-- source/Files.lua
+
+local lfs = require("lfs")  -- Ensure LuaFileSystem is available
+
+-- Function to create a directory if it doesn't exist
+local function createDirectory(path)
+    -- Check if the directory already exists
+    if lfs.attributes(path) then
+        print("Directory already exists: " .. path)
+        return true
+    else
+        -- Try to create the directory
+        local success, err = lfs.mkdir(path)
+        if success then
+            print("Directory created: " .. path)
+            return true
+        else
+            print("Error creating directory: " .. err)
+            return false
+        end
+    end
+end
+
+-- Function to initialize the Workspace directory
+local function initializeWorkspace(appName)
+    local path = "/var/mobile/Containers/Data/Application/" .. appName .. "/Documents/Workspace"
+    return createDirectory(path)
+end
+
+-- Return the functions to be used by other modules
+return {
+    initializeWorkspace = initializeWorkspace
+}

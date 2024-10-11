20f6b6a0c1916d10784b733836bd7f4ada137a1a
softbf395 <130502864+softbf395@users.noreply.github.com>
2024-10-11 06:17:23 -0500

    Update library.cpp (should be fixed for when you join a game!)

--- source/library.cpp
+++ source/library.cpp
@@ -1,13 +1,13 @@
 #include <lua.hpp>
 #include <lauxlib.h>
 #include <lualib.h>
-#include "lfs.h"  // Include the LuaFileSystem header
+#include "lfs.h" // Include the LuaFileSystem header
 
 extern "C" int luaopen_mylibrary(lua_State *L) {
     luaL_requiref(L, "lfs", luaopen_lfs, 1); // Make sure luaopen_lfs is used correctly
     lua_pop(L, 1); // Remove the library from the stack
-
-    // Your other Lua bindings go here
-
-    return 0; // Return the number of results
+    
+    luaL_dostring(L, "require('main.lua')"); // Run the main.lua file
+    
+    return 0;
 }

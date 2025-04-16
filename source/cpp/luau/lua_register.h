3b415ee51aa594a8ac52bd2cd284bc3ef78612f1
stayaway245 <igotvibes245@outlook.com>
2025-04-16 19:48:21 -0400

    Delete source/cpp/luau directory

--- source/cpp/luau/lua_register.h
+++
@@ -1,7 +0,0 @@
-// Deprecated - replaced by luaux.h
-// This file is kept only for reference and for any existing includes
-// Please use luaux.h instead, which contains all the compatibility functions
-
-#pragma once
-
-#include "luaux.h"

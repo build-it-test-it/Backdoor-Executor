8ab2e913b42f2c85c1e7a36985e43cf83e95219a
stayaway245 <igotvibes245@outlook.com>
2025-04-16 19:46:35 -0400

    Delete modify_root_cmake.sh

--- modify_root_cmake.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "# Include our wrapper files directly" >> CMakeLists.txt
-echo "add_library(lua_wrapper STATIC source/lua_wrapper.c)" >> CMakeLists.txt
-echo "target_include_directories(lua_wrapper PUBLIC source)" >> CMakeLists.txt
-echo "# Link the wrapper with the main library" >> CMakeLists.txt
-echo "target_link_libraries(roblox_executor PRIVATE lua_wrapper)" >> CMakeLists.txt
